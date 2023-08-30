08ab4fe851ba59776b11c481852e4cdc5a2337ab
Nusrat Parvin <nusrat.shumona@gmail.com>
2023-08-30 23:21:52 +0600

    data loaded

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log('shum');
+const loadData = async() =>{
+    const url = `https://openapi.programming-hero.com/api/ai/tools`;
+    const res = await fetch(url);
+    const data = await res.json();
+    displayData(data.data);
+}
+
+const displayData = (item) =>{
+    
+}
+
+loadData();
